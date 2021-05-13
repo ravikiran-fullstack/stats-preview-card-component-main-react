8ac9036fdf7100a436403bd63f749ad30953b1da
Ravikiran Yadava <ravikiransjce.jobs@gmail.com>
2021-05-13 14:02:09 +0530

    Created stats preview card, need to work on responsiveness

---
+++ src/components/statsPreviewCard/StatsPreviewCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import "./styles.css";
+
+const StatsPreviewCard = () => {
+  return (
+    <div className="row center card-container">
+      <div className="col-2 card-body">
+        <div className="heading">
+          <h1>
+            Get <span className="heading-insight">insights</span> that help{" "}
+            <br />
+            your business grow
+          </h1>
+        </div>
+        <div className="info">
+          <p>
+            Discover the benefits of data analytics and make better decisions
+            regarding revunue, customer experience and overall efficiency
+          </p>
+        </div>
+        <div className="row company-stats">
+          <div className="col-1">
+            <h1>10k+</h1>
+            <p>companies</p>
+          </div>
+          <div className="col-1">
+            <h1>314</h1>
+            <p>templates</p>
+          </div>
+          <div className="col-1">
+            <h1>12M+</h1>
+            <p>queries</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-2 card-image background-tint"></div>
+    </div>
+  );
+};
+
+export default StatsPreviewCard;
